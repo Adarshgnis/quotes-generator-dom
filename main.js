2e6c2db7dddcee2ec8405726418cef6e0347e80e
- <=>
2023-09-09 19:50:22 +0530

    almost done

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let container = document.querySelector("#quote-container")
+let backgroundBtn = document.querySelector("#colorchangerbtn")
 let downloadbtn = document.querySelector('#downloadbtn')
 let quoteInput = document.querySelector('[name=quote]')
 let quoteText = document.querySelector('#quote')
-let backgroundBtn = document.querySelector("#colorchangerbtn")
+let authorInput = document.querySelector('[name=author]')
+let authorText = document.querySelector('#author')
 
 
 // Background color change
@@ -11,3 +13,22 @@ backgroundBtn.addEventListener("click", function(a){
     container.style.background = randomColors
 })
 
+// manipulate the text
+quoteInput.addEventListener('keyup', function(a){
+    quoteText.innerText = a.currentTarget.value
+})
+
+authorInput.addEventListener('keyup', function(a){
+    authorText.innerText = a.currentTarget.value
+})
+
+
+
+downloadbtn.addEventListener('click', function() {
+    html2canvas(container).then(canvas => {
+        let a = document.createElement("a")
+        a.download = Math.floor(Math.random() * 3000) + ".png"
+        a.href = canvas.toDataURL()
+        a.click()
+    })
+})
